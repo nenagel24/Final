62ad756ef0cfef45843488601a2fca559fbb14f6
nenagel24 <ttrnick14@yahoo.com>
2016-03-28 10:17:30 -0400

    Added files via upload

---
+++ Characters.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class characters{
+private:
+	//main character(user)
+	int character;
+	int characterHealth;
+	int characterAttack;
+	//First Boss
+	int baas;
+	int baasHealth;
+	int baasAttack;
+	//Second Boss
+	int megatron;
+	int megatronHealth;
+	int megatronArmor;
+	int megatronAttack;
+	//Third Boss
+
+
+
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	
+
+
+
+
+
+	return 0;
+	system("pause");
+}

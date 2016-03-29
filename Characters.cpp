75cfd7b63444a3d8ce9e33287f18194c0c6076c2
nenagel24 <ttrnick14@yahoo.com>
2016-03-29 10:24:33 -0400

    Added files via upload

---
+++ Characters.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Characters.h"
+
+
+int characters::getCharacter(){
+	return character;
+}
+int characters::getCharacterHealth(){
+	return characterHealth;
+}
+int characters::getCharacterAttack(){
+	return characterAttack;
+}
+
+int characters::getCharacterArmor(){
+	return characterArmor;
+}
+
+int characters::getBandit(){
+	return bandit;
+}
+
+int characters::getBanditHealth(){
+	return banditHealth;
+}
+
+int characters::getBanditAttack(){
+	return banditAttack;
+}
+
+int characters::getBaas(){
+	return baas;
+}
+
+int characters::getBaasHealth(){
+	return baasHealth;
+}
+
+int characters::getBaasAttack(){
+	return baasAttack;
+}
+
+int characters::getMegatron(){
+	return megatron;
+}
+
+int characters::getMegatronHealth(){
+	return megatronHealth;
+}
+
+int characters::getMegatronArmor(){
+	return megatronArmor;
+}
+
+int characters::getMegatronAttack(){
+	return megatronAttack;
+}
+
+int characters::getHound(){
+	return hound;
+}
+
+int characters::getHoundHealth(){
+	return houndHealth;
+}
+
+int characters::getHoundAttack(){
+	return houndAttack;
+}
+
+int characters::getZombiehoard(){
+	return zombiehoard;
+}
+
+int characters::getZombiehoardHealth(){
+	return zombiehoardHealth;
+}
+
+int characters::getZombiehoardAttack(){
+	return zombiehoardAttack;
+}
+
+int characters::getTrex(){
+	return trex;
+}
+
+int characters::getTrexHealth(){
+	return trexHealth;
+}
+
+int characters::getTrexAttack(){
+	return trexAttack;
+}
+
+int characters::getHades(){
+	return Hades;
+}
+
+int characters::getHadesHealth(){
+	return HadesHealth;
+}
+
+int characters::getHadesArmor(){
+	return HadesArmor;
+}
+
+int characters::getHadesAttack(){
+	return HadesAttack;
+}
+
+int characters::getZeus(){
+	return zeus;
+}
+
+int characters::getZeusHealth(){
+	return zeusHealth;
+}
+
+int characters::getZeusArmor(){
+	return zeusArmor;
+}
+
+int characters::getZeusAttack(){
+	return zeusAttack;
+}

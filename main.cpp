d0a9513d6dbfd967613206b108e386cafeac2e15
nenagel24 <ttrnick14@yahoo.com>
2016-04-13 14:28:58 -0400

    Shop Menu
    Did the better part of the shop menu along with some new variables

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ int main(){
 	string name;
 	characters characters;
 	int mainMenu;
+	int shopSelect;
 	while (true){
 
 		cout << "Welcome to C++ RPG " << endl;
@@ -30,7 +31,7 @@ int main(){
 
 
 		cout << "Main Menu " << endl;
-		cout << "Select what you want to do:  ";
+		cout << "Select what you want to do:  " << endl;
 		cout << "1. Prologue " << endl;
 		cout << "2. Shop " << endl;
 		cout << "3. Fight Bandits " << endl;
@@ -47,22 +48,40 @@ int main(){
 			cout << "Your father was known for his skill of martial arts and his brute strength " << endl;
 			cout << "He died fighting for your village against Zeus, the God of lightning " << endl;
 			cout << "You now seek revenge on Zeus for killing your father and kidnapping your younger siblings " << endl;
+			mainMenu - 1;
 		case 2:
+			cout << "Welcome to Rons Shop of the Underworld! " << endl;
+			cout << "Your balance is: " << money << endl;
+			cout << "1. Warriors Shield(+10) Defense " << endl;
+			cout << "2. Hermes Blade(+12) Attack " << endl;
+			cout << "3. Knights armor(+20) Armor  " << endl;
+			cout << "4. Healing Potion(+15) Health " << endl;
+			cout << "5. Rage Potion(+15) Attack 3 moves only " << endl;
+			cout << "6. Speed Potion, One use, allows battle flee " << endl;
+			cout << "7. Ingrams Steel Shield(+20) Defense " << endl;
+			cout << "8. Kryptonite Blade(+50) Attack " << endl;
+			cout << "9. Gold Titanium Alloy Suit(+70) Armor " << endl;
+			cout << "Select an item: " << endl;
+			cin >> shopSelect;
 		case 3:
+			cout << endl;
 		case 4:
+			cout << endl;
 		case 5:
+			cout << endl;
 		case 6:
+			cout << endl;
 		case 7:
+			cout << endl;
 		case 8:
+			cout << endl;
 		case 9:
+			cout << endl;
 		case 10:
+			cout << endl;
 		
 		}
 	}
-	
-
-
-
 
 	return 0;
 	system("pause");

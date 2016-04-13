ad1c8c74747d703c3ff7270e82bd0cd3b04af803
nenagel24 <ttrnick14@yahoo.com>
2016-04-13 14:27:29 -0400

    Added Shop
    Added getters for the shop and most of the items that I plan on having.

---
+++ Shop.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+
+class Shop{
+private:
+	int warriorShield;
+	int hermesBlade;
+	int knightArmor;
+	int healingPotion;
+	int ragePotion;
+	int speedPotion;
+	int ingramsShield;
+	int kryptoBlade;
+	int goldSuit;
+public:
+	int getWarriorShield();
+	int getHermesBlade();
+	int getKnightArmor();
+	int getHealingPotion();
+	int getRagePotion();
+	int getSpeedPotion();
+	int getIngramsShield;
+	int getKryptoBlade;
+	int getGoldSuit;
+};
